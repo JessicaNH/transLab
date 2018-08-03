a4f72227908deeece6455d091eb101d27f8757ca
Jessica Navarro <jess.nav359.herrera@gmail.com>
2018-08-03 10:56:24 -0400

    subiendo cambios de test

--- src/test/validate.specs.js
+++ src/test/validate.specs.js
@@ -5,44 +5,60 @@ require('../src/js/validate'); //Sólo enlaza el archivo, parecido al script de 
 describe('Validar password', ()=>{ //Describe lo que habrá dentro de cada función
     describe('Debería verificar si tiene un maximo caracteres', ()=>{
       it('Debería el password tener un maximo de 8 numeros', ()=>{ //Caso de prueba
-        assert.equal(validatePassword("12345678"), true); //assert.equal verifica que el parámetro 1 sea igual al parámetro 2
+        assert.equal(validatePassword(12345678), true); //assert.equal verifica que el parámetro 1 sea igual al parámetro 2
         assert.equal(validatePassword("jfgk1234"), false);
         assert.equal(validatePassword("ajskeoldfjrnvmk"), false);            
-    });
+      });
         it('Debería el password tener solo numeros', ()=>{ //Caso de prueba
-            assert.equal(validatePassword("12345678"), true); //assert.equal verifica que el parámetro 1 sea igual al parámetro 2
+            assert.equal(validatePassword(12345678), true); //assert.equal verifica que el parámetro 1 sea igual al parámetro 2
             assert.equal(validatePassword("jfgk1234"), false);
             assert.equal(validatePassword("ajskeold"), false);            
         });
         
-});
+    });
     
 
 });
 describe('Validar tarjetabip', ()=>{ //Describe lo que habrá dentro de cada función
   describe('Debería verificar si tiene un maximo caracteres', ()=>{
     it('Debería tener un total de 8 numeros', ()=>{ //Caso de prueba
-      assert.equal(validatePassword("12345678"), true); //assert.equal verifica que el parámetro 1 sea igual al parámetro 2
-      assert.equal(validatePassword("jfgk1234"), false);
-      assert.equal(validatePassword("ajskeoldfjrnvmk"), false);
-      assert.equal(validatePassword("123"), false); 
-                 
-  });
-    it('Debería el password tener solo numeros', ()=>{ //Caso de prueba
-    assert.equal(validatePassword("12345678"), true); //assert.equal verifica que el parámetro 1 sea igual al parámetro 2
-    assert.equal(validatePassword("jfgk1234"), false);
-    assert.equal(validatePassword("ajskeold"), false);            
+      assert.equal(validateBip(12345678), true); //assert.equal verifica que el parámetro 1 sea igual al parámetro 2
+      assert.equal(validateBip("jfgk1234"), false);
+      assert.equal(validateBip("ajskeoldfjrnvmk"), false);
+      assert.equal(validatebBip(123), false);
+    });                 
+
+    it('Debería la tarjeta tener solo numeros', ()=>{ //Caso de prueba
+      assert.equal(validateBip(12345678), true); //assert.equal verifica que el parámetro 1 sea igual al parámetro 2
+      assert.equal(validateBip("jfgk1234"), false);
+      assert.equal(validateBip("ajskeold"), false);            
+    }); 
+  });      
+      
+});
+
+describe('calcular tarifa', ()=>{ 
+  describe('calculate', ()=>{
+    it('Debería ser una funcion', ()=>{
+      assert.equal(typeof calculate, 'function'); 
+                  
     });
+    it('Debería retornar 40 para el saldo inicial 800 con tarifa alta 760', ()=>{ 
+    assert.equal(validatePassword("12345678"), true); 
+    assert.equal(validatePassword("jfgk1234"), false);
+               
+    });
+  });
       
       
 });
+
 
 
 
 
   
 
-});
 
 
 
