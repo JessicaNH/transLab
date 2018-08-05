f4d3a9bfa199d67c06ba9a9fe52a76a8927d7347
Jessica Navarro <jess.nav359.herrera@gmail.com>
2018-08-05 16:18:08 -0400

    validando mail y password

---
+++ assets/js/validate.js
@@ -0,0 +1,22 @@
+const btnInit = document.getElementById('start');
+// Validar mail y passwords
+btnInit.addEventListener('click', () => { 
+  let email = document.getElementById('email').value;
+  let password = document.getElementById('password').value;
+  const expresion = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+  
+  if(email === "" || password === "") {
+    alert("Todos los campos son obligatorios");
+  }
+  else if(!expresion.test(email)) {
+    alert("El correo no es valido")
+  }
+  else if(password.length > 8) {
+    alert("La contaseña debe tener 8 caracteres");
+  }
+  else if(isNaN(password)){
+    alert("Debe ser solo números");
+    return false;
+  }
+  
+}); 
